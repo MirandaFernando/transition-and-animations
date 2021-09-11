95e2b0f1f359df7d334366eff7076b25af44fabf
Fernando <fernandomirandalolzeiro@gmail.com>
2021-09-11 18:27:10 -0300

    update css

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 (() => {
-    const containerMain = document.querySelector('.t2')
-    const containerSecondary = document.querySelector('.t1')
-  
-    setTimeout(() => {
-      containerMain.classList.toggle('t2--hide')
-      containerSecondary.classList.toggle('t1--hide')
-    }, 10000)
+  const containerMain = document.querySelector('.t2')
+  const containerSecondary = document.querySelector('.t1')
+
+  setTimeout(() => {
+    containerMain.classList.toggle('t2--hide')
+    containerSecondary.classList.toggle('t1--hide')
+  }, 10000)
 })()
